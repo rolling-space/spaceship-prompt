aaa4591cea661f2b7eabd7f64718b07e0bafed4c
Maxim Baz <git@maximbaz.com>
2018-05-29 10:15:37 +0200

    Make spaceship async, flatten git sections

--- sections/kubecontext.zsh
+++ sections/kubecontext.zsh
@@ -14,47 +14,28 @@ SPACESHIP_KUBECONTEXT_PREFIX="${SPACESHIP_KUBECONTEXT_PREFIX="at "}"
 SPACESHIP_KUBECONTEXT_SUFFIX="${SPACESHIP_KUBECONTEXT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_KUBECONTEXT_SYMBOL="${SPACESHIP_KUBECONTEXT_SYMBOL="☸️ "}"
 SPACESHIP_KUBECONTEXT_COLOR="${SPACESHIP_KUBECONTEXT_COLOR="cyan"}"
-SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW="${SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW=true}"
-SPACESHIP_KUBECONTEXT_COLOR_GROUPS=(${SPACESHIP_KUBECONTEXT_COLOR_GROUPS=})
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-# Show current context in kubectl
-spaceship_kubecontext() {
+spaceship_async_job_load_kubecontext() {
   [[ $SPACESHIP_KUBECONTEXT_SHOW == false ]] && return
 
-  spaceship::exists kubectl || return
+  async_job spaceship spaceship_async_job_kubecontext
+}
 
-  local kube_context=$(kubectl config current-context 2>/dev/null)
-  [[ -z $kube_context ]] && return
+spaceship_async_job_kubecontext() {
+  echo $(kubectl config current-context 2>/dev/null)
+}
 
-  if [[ $SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW == true ]]; then
-    local kube_namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)
-    [[ -n $kube_namespace && "$kube_namespace" != "default" ]] && kube_context="$kube_context ($kube_namespace)"
-  fi
-
-  # Apply custom color to section if $kube_context matches a pattern defined in SPACESHIP_KUBECONTEXT_COLOR_GROUPS array.
-  # See Options.md for usage example.
-  local len=${#SPACESHIP_KUBECONTEXT_COLOR_GROUPS[@]}
-  local it_to=$((len / 2))
-  local 'section_color' 'i'
-  for ((i = 1; i <= $it_to; i++)); do
-    local idx=$(((i - 1) * 2))
-    local color="${SPACESHIP_KUBECONTEXT_COLOR_GROUPS[$idx + 1]}"
-    local pattern="${SPACESHIP_KUBECONTEXT_COLOR_GROUPS[$idx + 2]}"
-    if [[ "$kube_context" =~ "$pattern" ]]; then
-      section_color=$color
-      break
-    fi
-  done
-
-  [[ -z "$section_color" ]] && section_color=$SPACESHIP_KUBECONTEXT_COLOR
+# Show current context in kubectl
+spaceship_kubecontext() {
+  [[ -z "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_kubecontext]}" ]] && return
 
   spaceship::section \
-    "$section_color" \
+    "$SPACESHIP_KUBECONTEXT_COLOR" \
     "$SPACESHIP_KUBECONTEXT_PREFIX" \
-    "${SPACESHIP_KUBECONTEXT_SYMBOL}${kube_context}" \
+    "${SPACESHIP_KUBECONTEXT_SYMBOL}${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_kubecontext]}" \
     "$SPACESHIP_KUBECONTEXT_SUFFIX"
 }
